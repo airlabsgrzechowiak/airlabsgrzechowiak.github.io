fc7bde9be0782827da876a95357bad41459cafa3
krzysztofengineer <krzysztof@engineer.com>
2018-02-06 11:42:57 +0100

    Seems responsive

--- src/app.js
+++ src/app.js
@@ -4,10 +4,21 @@ try {
     window.$ = window.jQuery = require('jquery');
 } catch (e) {}
 
+window.Vue = require('vue');
+Vue.use(require('vue-scrollto'), { offset: -50});
+
 import ScrollReveal from "scrollreveal";
 window.sr = ScrollReveal();
 
+const app = new Vue({
+    el: '#app'
+});
+
 $(document).ready(function() {
-    sr.reveal('.reveal-1', { delay: 500 });
-    sr.reveal('.reveal-1-sequence', { delay: 500 }, 300);
+    sr.reveal('.rv1', { delay: 500 });
+    sr.reveal('.rv2', { delay: 700 });
+    sr.reveal('.rv3', { delay: 1100 });
+    sr.reveal('.rv4', { delay: 1400 });
+    sr.reveal('.rv5', { delay: 2000 });
+    sr.reveal('.rseq', { delay: 300 }, 300);
 });
